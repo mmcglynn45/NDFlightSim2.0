266143051ce2a67ff41fc984635e2567c8302e12
Matt McGlynn <Sean@no.local>
2013-03-29 19:13:20 -0500

    Initial Commit

---
+++ FinalProjectFund2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FinalProjectFund2
+//
+//  Created by Matt McGlynn on 3/29/13.
+//  Copyright (c) 2013 Matt McGlynn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
